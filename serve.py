38581d84e2986c10e2bb1433ca90ac4aa180597a
Rachael Tatman <rctatman@users.noreply.github.com>
2019-04-17 16:17:19 -0700

    Update serve.py

--- serve.py
+++ serve.py
@@ -1,15 +1,5 @@
-from flashtext.keyword import KeywordProcessor
-import pickle
-
-# funtion that takes in a text string & returns "NER" output
-def get_keywords_api():
-    # read in pickled word processor
-    with open("word_processor.pkl", "rb") as file:
-        keyword_processor = pickle.load(file)
-
-    # get our keywords
-    def keywords_api(text):
-        keywords_found = keyword_processor.extract_keywords(text, span_info=True)
-        return keywords_found
-
-    return keywords_api
+def useless_function():
+    """ 
+    A function that prints some text
+    """
+    print("some text")
